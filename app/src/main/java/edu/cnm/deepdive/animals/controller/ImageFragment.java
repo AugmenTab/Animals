fd7a8009b81be55ea07d69d68ee908b8e67ce9ce
AugmenTab <tbaum92@live.com>
2020-10-07 12:06:08 -0600

    Created Randomizer button.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -7,6 +7,7 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +22,7 @@ import edu.cnm.deepdive.animals.service.AnimalService;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -28,12 +30,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ImageFragment extends Fragment {
 
   private WebView contentView;
+  private List<Animal> animals;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_image, container, false);
     setupWebView(root);
+    root.findViewById(R.id.randomize).setOnClickListener((v) -> randomize());
     return root;
   }
 
@@ -52,6 +56,12 @@ public class ImageFragment extends Fragment {
     settings.setDisplayZoomControls(false);
     settings.setLoadWithOverviewMode(true);
     new RetrieverTask().execute();
+  }
+
+  private void randomize() {
+    Random rng = new Random();
+    final String imageUrl = animals.get(rng.nextInt(animals.size())).getImageUrl();
+    contentView.loadUrl(imageUrl);
   }
 
   private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
@@ -92,14 +102,8 @@ public class ImageFragment extends Fragment {
 
     @Override
     protected void onPostExecute(List<Animal> animalList) {
-      final String imageUrl = animalList.get(0).getImageUrl();
-
-      Objects.requireNonNull(getActivity()).runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-          contentView.loadUrl(imageUrl);
-        }
-      });
+      animals = animalList;
+      randomize();
     }
 
   }
